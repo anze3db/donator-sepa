dd1b2e3a3207763a8dd64b9170d2fd0b9aa50ada
smotko <smotko@smotko-debian.smotko-debian.com>
2013-01-09 17:45:18 +0100

    Set production server

--- settings.py
+++ settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'NAME': 'donator',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '192.168.1.91',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '192.168.1.10',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
